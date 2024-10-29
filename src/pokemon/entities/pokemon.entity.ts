782432d984c8894138c2e2025922a11c3257a34a
jdsuarez <jdsuarez@fgs.co>
2024-10-29 11:10:52 -0500

    adding the conection to mongoose

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,27 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+  type: string;
+  @Prop({
+    index: true,
+    max: 10,
+  })
+  level: number;
+  @Prop({
+    index: true,
+  })
+  trainer: string;
+  abilities: string[];
+  @Prop({
+    index: true,
+  })
+  dateCaught: Date;
+}
+export const PockemonSchema = SchemaFactory.createForClass(Pokemon);
